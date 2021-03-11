c0bc53b7ed05dd58598106c890eb9d90aafe9138
leandrobruzzi <leandrobruzzi@gmail.com>
2021-03-11 14:42:50 -0300

    Agrego configuracion de log4j para mostrar CID  y messages.properties.

---
+++ src/main/java/ar/com/meli/cupon/config/Config.java
@@ -0,0 +1,30 @@
+package ar.com.meli.cupon.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+    @Autowired
+    private CorrelationInterceptor correlationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(correlationInterceptor);
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
